580379059149729fdc18d932db7c6fb8c278ca70
Xavier Hermand <xavier@adaltas.com>
2018-10-01 13:07:21 +0200

    Remove Node.js 4 test for Archlinux

--- docker/archlinux/run.sh
+++ docker/archlinux/run.sh
@@ -7,7 +7,7 @@ cd /node-krb5
 cat /tmp/krb5_test/krb5.conf
 cp /tmp/krb5_test/krb5.conf /etc/krb5.conf
 npm run krb5-lib
-for node_version in "10.11.0" "8.12.0" "6.14.4" "4.9.1"; do
+for node_version in "10.11.0" "8.12.0" "6.14.4"; do
   echo "Node.js version "$node_version
   n $node_version
   # --unsafe-perm allows node-gyp build as root
@@ -23,5 +23,5 @@ for node_version in "10.11.0" "8.12.0" "6.14.4" "4.9.1"; do
     echo -e $GREEN"Tests passed for Node.js "$node_version$NC
   fi
 done
-echo -e $GREEN"Tests passed for Node.js version 4, 6, 8 and 10"$NC
+echo -e $GREEN"Tests passed for Node.js version 6, 8 and 10"$NC
 exit 0

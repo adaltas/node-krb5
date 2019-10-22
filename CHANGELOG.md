19f5149ed060e5cc16b7c3f0ab79f4c70229bf79
David Worms <david@adaltas.com>
2019-10-23 00:22:04 +0200

    test: improve error message in docker run command

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+
+# Changelog
+
+## Version 0.5.2
+
+* test: improve error message in docker run command
--- docker/run_tests.sh
+++ docker/run_tests.sh
@@ -2,7 +2,7 @@
 
 set +e
 if [ "$#" -eq 0 ];then
-	echo "No environment set"
+	echo "No environment set: archlinux, centos7, ubuntu"
   exit 1
 fi
 

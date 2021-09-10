7e42a10d87e6b9b4389d63b83a59e691dac9f26b
Guillaume Boutry <boutryguillaume1@gmail.com>
2021-09-10 21:40:40 +0200

    Fix docker tests (#26)
    * FIX ius repositories have been moved
    
    * FIX archlinux base image has been moved
    
    * FIX tests, ADD node 16
    
    Bash array definition was faulty, which made the first "n" call fail.
    Version "12," doesn't exist, but the tests where still correctly executed
    because the last installed version of node was 12.
    
    * FIX update centos7 toolchain
    
    Compiling against node 16 needs the option --std=c++14, which has been added to GCC-C++ 4.9, default gcc version is 4.8.5 in centos7
    
    * DEL remove older node versions ADD node 14 tests

--- docker/centos7/run.sh
+++ docker/centos7/run.sh
@@ -6,6 +6,8 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
+source scl_source enable devtoolset-10 # load environment configuration to use gcc 7
+
 while [ ! -f /tmp/krb5_test/rest_ready ] 
 do
   echo "Waiting for REST server to be ready..."
@@ -13,7 +15,7 @@ do
 done
 cd /node-krb5
 cp /tmp/krb5_test/krb5.conf /etc/krb5.conf
-for node_version in "12.10.0", "10.16.3" "8.16.1" "6.17.1"; do
+for node_version in "16" "14"; do
   echo "Node.js version "$node_version
   n $node_version
   # --unsafe-perm allows node-gyp build as root
@@ -29,5 +31,5 @@ for node_version in "12.10.0", "10.16.3" "8.16.1" "6.17.1"; do
     echo -e $GREEN"Tests passed for Node.js "$node_version$NC
   fi
 done
-echo -e $GREEN"Tests passed for Node.js version 6, 8, 10 and 12"$NC
+echo -e $GREEN"Tests passed for Node.js version 14 and 16"$NC
 exit 0
